05f9930225ed32753bb9f9ea6c039ac9e9cfe868
nbbosco <nickilu009@gmail.com>
2022-01-03 19:54:53 -0300

    NB_React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Index from './components/index'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Index />
+      </main>
+    </div>
+  );
+}
+
+export default App;
