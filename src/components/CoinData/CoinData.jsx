f4c75dc641dd25c95d602167ae17a9ac11e1229f
Martinmeza0 <martinmezacorreo@gmail.com>
2022-01-12 17:29:57 -0300

    UPDATE: className for css

--- src/components/CoinData/CoinData.jsx
+++ src/components/CoinData/CoinData.jsx
@@ -5,50 +5,46 @@ const CoinData = ({ data }) => {
     if (data) {
       return (
         <>
-          <div className="">
-            <div className="">
-              <span className="">Market Cap</span>
+          <div>
+            <div>
+              <span>Market Cap</span>
               <span>{data.market_cap}</span>
             </div>
-
-            <hr />
             
-            <div className="d-flex flex-column">
-              <span className="text-muted coin-data-category">
+            <div className="column">
+              <span>
                 Total Supply
               </span>
               <span>{data.total_supply}</span>
             </div>
           </div>
 
-          <div className="col-sm">
-            <div className="d-flex flex-column">
-              <span className="text-muted coin-data-category">Volume(24H)</span>
+          <div>
+            <div className="column">
+              <span>Volume(24H)</span>
               <span>{data.total_volume}</span>
             </div>
             <hr />
-            <div className="d-flex flex-column">
-              <span className="text-muted coin-data-category">high 24h</span>
+            <div className="column">
+              <span>high 24h</span>
               <span>{data.high_24h}</span>
             </div>
           </div>
 
-          <div className="col-sm">
-            <div className="d-flex flex-column">
-              <span className="text-muted coin-data-category">
+          <div>
+            <div className="column">
+              <span>
                 Circulating Supply
               </span>
               <span>{data.circulating_supply}</span>
             </div>
             <hr />
-            <div className="d-flex flex-column">
-              <span className="text-muted coin-data-category">low 24h</span>
+            <div className="column">
+              <span>low 24h</span>
               <span>{data.low_24h}</span>
             </div>
           </div>
         </>
-
-        
       );
     }
   };
