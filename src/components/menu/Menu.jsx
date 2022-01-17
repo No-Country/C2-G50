29bba6d4efbb3f7ac8b50b9a8348a7671f462da5
Martinmeza0 <martinmezacorreo@gmail.com>
2022-01-17 19:44:47 -0300

    UPDATE: Fixed position added

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,26 +7,22 @@ const [openNav, setOpenNav] = useState(false);
 
 return (
   <>
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/"><span class="iconify" data-icon="mdi:view-dashboard"></span> Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/portafolio"><span class="iconify" data-icon="bx:bxs-wallet"></span> Portafolio</Link>
-          </li>
-          <li>
-            {/* <Link to="/news"><span class="iconify" data-icon="wpf:globe-earth"></span> News</Link> */}
-            <Link to="/news"><span class="iconify" data-icon="bx:bx-world"></span> News</Link>
-
-          </li>
-          <li>
-            <Link to="/settings"><span class="iconify" data-icon="uiw:setting"></span> Settings</Link>
-          </li>
-        </ul>
-      </nav> 
-    </header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/"><span class="iconify" data-icon="mdi:view-dashboard"></span> Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/portafolio"><span class="iconify" data-icon="bx:bxs-wallet"></span> Portafolio</Link>
+        </li>
+        <li>
+          <Link to="/news"><span class="iconify" data-icon="bx:bx-world"></span> News</Link>
+        </li>
+        <li>
+          <Link to="/settings"><span class="iconify" data-icon="uiw:setting"></span> Settings</Link>
+        </li>
+      </ul>
+    </nav> 
   </>
   )
 }
