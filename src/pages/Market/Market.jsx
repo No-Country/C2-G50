7c7f773a77a1568a2eda566638313143c22613a4
Martinmeza0 <martinmezacorreo@gmail.com>
2022-01-21 09:35:28 -0300

    MOVE: to his respective folder

---
+++ src/pages/Market/Market.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import TableCoins from "../../components/CoinsTable/TableCoins";
+import Menu from "../../components/menu/Menu";
+import coinGecko from "../../api/coinGecko";
+import "./Market.css";
+import Top3Coins from "../../components/Top3Coins/Top3Coins";
+
+const Market = () => {
+  const [search, setSearch] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await coinGecko.get(
+        "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true"
+      );
+      setCoins(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <Menu />
+        <main className="dashboard__container row">
+          <Top3Coins coins={coins} />
+          <input
+            type="text"
+            placeholder="Search a Coin"
+            className="search"
+            autoFocus
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <TableCoins coins={coins} search={search} />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Market;
