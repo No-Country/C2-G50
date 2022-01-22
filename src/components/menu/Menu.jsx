a126505f8dc3bd690834d91c04df81ad5c2ab0ca
nbbosco <nickilu009@gmail.com>
2022-01-22 17:53:44 -0300

    NB_Dark/Light Mode

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import "./Menu.css";
+import "../../App.css";
 import { NavLink } from "react-router-dom";
+import useLocalStorage from 'use-local-storage'
 
 const Menu = () => {
   const [openNav, setOpenNav] = useState(false);
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : 'light';
+    setTheme(newTheme);
+  }
 
   return (
     <>
-      <nav>
+      <nav className='app' data-theme={theme}>
         <ul>
           <li>
             <NavLink exact to="/" activeClassName="navLink__active">
@@ -26,10 +35,11 @@ const Menu = () => {
               <span className="iconify" data-icon="bx:bx-world"></span> News
             </NavLink>
           </li>
-          <li>
-            <NavLink exact to="/settings" activeClassName="navLink__active">
-              <span className="iconify" data-icon="uiw:setting"></span> Settings
-            </NavLink>
+          <li >
+              <span className="iconify" data-icon="uiw:setting"></span>
+              <button onClick={switchTheme}>
+                {theme === 'light' ? 'Dark' : 'Light'} Theme
+              </button>
           </li>
         </ul>
       </nav>
