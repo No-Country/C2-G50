6b641d6acb1e4b88af1bb989e96964f0d9ec15b1
Martinmeza0 <martinmezacorreo@gmail.com>
2022-01-23 17:20:31 -0300

    changes

--- src/components/Top3Coins/Top3Coins.jsx
+++ src/components/Top3Coins/Top3Coins.jsx
@@ -16,31 +16,38 @@ const Top3Coins = ({ coins }) => {
   const sorted = [...coins].sort(compare).slice(0, 3);
 
   return (
-    <section className="top__coins">
-      {sorted.map((coin, index) => (
-        <div className={`top__coin top__coin--${index + 1}`} key={coin.id}>
-          <div className="coin__header">
-            <span className="coin__name">{coin.name}</span>
-            <span className="coin__symbol">({coin.symbol.toUpperCase()})</span>
-          </div>
-          <Link to={`/coins/${coin.id}`}>
-          <img src={coin.image} />
-          </Link>
-          <div className="coin__body">
-            <span className="current__price">
-              ${coin.current_price.toLocaleString()}
-            </span>
-            <span className="change24h positive">
-              <span
-                class="iconify"
-                data-icon="ant-design:caret-up-filled"
-              ></span>
-              {coin.price_change_percentage_24h.toFixed(2)}%
-            </span>
-          </div>
-        </div>
-      ))}
-    </section>
+    <>
+      <header className= "top3">
+        <section className="top__title"> 
+          <h2><span class="iconify"  data-icon="ic:baseline-trending-up"></span> Best rising cryptocurrency</h2>
+        </section>
+        <section className="top__coins">
+          {sorted.map((coin, index) => (
+            <div className={`top__coin top__coin--${index + 1}`} key={coin.id}>
+              <header className="coin__header">
+                <h3 className="coin__name">{coin.name}</h3>
+                <span className="coin__symbol">({coin.symbol.toUpperCase()})</span>
+              </header>
+              <Link to={`/coins/${coin.id}`}>
+              <img src={coin.image} />
+              </Link>
+              <main className="coin__body">
+                <span className="current__price">
+                  ${coin.current_price.toLocaleString()}
+                </span>
+                <span className="change24h positive">
+                  <span
+                    class="iconify"
+                    data-icon="ant-design:caret-up-filled"
+                  ></span>
+                  {coin.price_change_percentage_24h.toFixed(2)}%
+                </span>
+              </main>
+            </div>
+          ))}
+        </section>
+      </header>
+    </>
   );
 };
 
